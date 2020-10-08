d1cda588ac1c13af3e485cfdd4c6992fbeefaeb2
Jamison Kellen <Jtkellen@protonmail.com>
2020-10-07 21:18:46 -0700

    Implement Functions

--- cse360assignment02/AddingMachine.java
+++ cse360assignment02/AddingMachine.java
@@ -4,19 +4,22 @@ package cse360assignment02;
  * and retain the commands in a string returned by toString()
  *
  * @author  Jamison Kellen
- * @version 1.0
+ * @version 2.0
  * @since   2020-10-7
  */
 public class AddingMachine {
   /**
    * @serial total is the running total of the values of all hitherto used
    * operations in AddingMachine.
+   * @serial saved is the saved string that toString() appends.
    */
   private int total;
+  private String saved;
   /**
    *  This is the default constructor
    */
   public AddingMachine() {
+    saved = "0";
     total = 0; //not needed - included for clarity
   }
   /**
@@ -25,7 +28,7 @@ public class AddingMachine {
    * @see total
    */
   public int getTotal() {
-    return 0;
+    return total;
   }
   /**
    * add adds the parameter value to the variable total
@@ -33,7 +36,8 @@ public class AddingMachine {
    * @param value this is the integer that will be added to toal
    */
   public void add(int value) {
-
+    saved += " + " + value;
+    total = total + value;
   }
   /**
    * subtract subtracts the parameter value from the variable total
@@ -41,7 +45,8 @@ public class AddingMachine {
    * @param value this is the integer that will be subtracted from total
    */
   public void subtract(int value) {
-
+    saved += " - " + value;
+    total = total - value;
   }
   /**
    * toString keeps a history of the transactions made starting at
@@ -50,13 +55,13 @@ public class AddingMachine {
    * @return string This returns a string with each opperation seperated by a space
    */
   public String toString() {
-    return "";
+    return saved;
   }
   /**
    * clear clears AddingMachine.
    */
   public void clear() {
-
+    total = 0;
+    saved = "0";
   }
-
 }
